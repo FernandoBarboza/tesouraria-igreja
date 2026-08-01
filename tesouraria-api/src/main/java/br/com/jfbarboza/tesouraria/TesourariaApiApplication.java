c6fc8d70776654544b024722f4ba020c47c28921
FernandoBarboza <fernando.barboza18@gmail.com>
2026-07-31 23:04:32 -0300

    Inicializa projeto spring boot com suporte ao postgresql

---
+++ tesouraria-api/src/main/java/br/com/jfbarboza/tesouraria/TesourariaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jfbarboza.tesouraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesourariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesourariaApiApplication.class, args);
+	}
+
+}
